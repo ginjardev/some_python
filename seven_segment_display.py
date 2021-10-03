ae87061f63d63ef6d573f7f1759174898c2acbb2
Iniubong Arthur <arthurshur@gmail.com>
2021-10-03 22:31:41 +0100

    seven segment display

--- seven_segment_display.py
+++ seven_segment_display.py
@@ -22,3 +22,47 @@ def seven_segment(number):
         print("  ".join(segment[i] for segment in digits))
 
 
+seven_segment(6)
+lines = [ '' for lin in range(5) ]
+print(lines)
+
+digits = [
+    '1111110', 
+    '0110000',
+    '1101101',	
+	'1111001',
+	'0110011',	
+	'1011011',	
+	'1011111',	
+	'1110000',	
+	'1111111',	
+	'1111011',
+]
+
+def print_number(num):
+    global digits
+    digs = str(num)
+    lines = lines = [ '' for lin in range(5) ]
+    for d in digs:
+        segs = [[' ', ' ', ' '] for lin in range(5)]
+        ptrn = digits[ord(d) - ord('0')]
+        if ptrn[0] == '1':
+            segs[0][0] = segs[0][1] = segs[0][2] = '#'
+        if ptrn[1] == '1':
+            segs[0][2] = segs[1][2] = segs[2][2] = '#'
+        if ptrn[2] == '1':
+            segs[2][2] = segs[3][2] = segs[4][2] = '#'
+        if ptrn[3] == '1':
+            segs[4][0] = segs[4][1] = segs[4][2] = '#'
+        if ptrn[4] == '1':
+            segs[2][0] = segs[3][0] = segs[4][0] = '#'
+        if ptrn[5] == '1':
+            segs[0][0] = segs[1][0] = segs[2][0] = '#'
+        if ptrn[6] == '1':
+            segs[2][0] = segs[2][1] = segs[2][2] = '#'
+        for lin in range(5):
+            lines[lin] += ''.join(segs[lin]) + ' '
+    for lin in lines:
+        print(lin)
+
+print_number(int(input("Enter the number you wish to display: ")))        
