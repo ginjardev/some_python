8fe81bbf2041f0de3d34fe68babe087f379f836f
Iniubong Arthur <arthurshur@gmail.com>
2021-10-05 00:00:52 +0100

    fixed error

--- numbers_processor.py
+++ numbers_processor.py
@@ -1,10 +1,10 @@
-numbers_string= input("Enter numbers separated with spaces: ")
+numbers_string = input("Enter numbers separated with spaces: ")
 numbers = numbers_string.split()
 total_sum = 0
 
 try:
     for number in numbers:
-        total_sum += number
+        total_sum += int(number)
     print('the total sum of numbers is ', total_sum)
 except:
     print(number, 'is not a number')
